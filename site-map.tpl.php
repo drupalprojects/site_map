2fb0097e97234681e271f7db7b97402727e09592
Amit Goyal <amitg@tekritisoftware.com>
2012-10-15 20:00:57 +0530

    Issue #1594280 by chriscohen: E_NOTICE errors

--- site-map.tpl.php
+++ site-map.tpl.php
@@ -23,55 +23,55 @@
 ?>
 
 <div id="site-map">
-  <?php if($message): ?>
+  <?php if (!empty($message)): ?>
     <div class="site-map-message">
       <?php print $message; ?>
     </div>
   <?php endif; ?>
 
-  <?php if($rss_legend): ?>
+  <?php if (!empty($rss_legend)): ?>
     <div class="site-map-rss-legend">
       <?php print $rss_legend; ?>
     </div>
   <?php endif; ?>
 
-  <?php if($front_page): ?>
+  <?php if (!empty($front_page)): ?>
     <div class="site-map-front-page">
       <?php print $front_page; ?>
     </div>
   <?php endif; ?>
 
-  <?php if($blogs): ?>
+  <?php if (!empty($blogs)): ?>
     <div class="site-map-blogs">
       <?php print $blogs; ?>
     </div>
   <?php endif; ?>
 
-  <?php if($books): ?>
+  <?php if (!empty($books)): ?>
     <div class="site-map-books">
       <?php print $books; ?>
     </div>
   <?php endif; ?>
 
-  <?php if($menus): ?>
+  <?php if (!empty($menus)): ?>
     <div class="site-map-menus">
       <?php print $menus; ?>
     </div>
   <?php endif; ?>
 
-  <?php if($faq): ?>
+  <?php if (!empty($faq)): ?>
     <div class="site-map-faq">
       <?php print $faq; ?>
     </div>
   <?php endif; ?>
 
-  <?php if($taxonomys): ?>
+  <?php if (!empty($taxonomys)): ?>
     <div class="site-map-taxonomys">
       <?php print $taxonomys; ?>
     </div>
   <?php endif; ?>
 
-  <?php if($additional): ?>
+  <?php if (!empty($additional)): ?>
     <div class="site-map-additional">
       <?php print $additional; ?>
     </div>
